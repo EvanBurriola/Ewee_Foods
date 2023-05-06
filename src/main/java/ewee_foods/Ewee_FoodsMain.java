097db05e53fc961632785c45f318709a405bc1b8
Evan Burriola <evanburriola12@tamu.edu>
2023-05-06 17:50:06 -0500

    Adds salt item to the game. No crafting or loot tables yet.

---
+++ src/main/java/ewee_foods/Ewee_FoodsMain.java
@@ -0,0 +1,40 @@
+package ewee_foods;
+
+import com.mojang.logging.LogUtils;
+import ewee_foods.item.ModItems;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("ewee_foods")
+public class Ewee_FoodsMain
+{
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "ewee_foods";
+
+    public Ewee_FoodsMain()
+    {
+        // Register the setup method for modloading
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
