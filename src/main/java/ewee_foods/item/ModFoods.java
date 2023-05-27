ea3ad60b4684603cfbf9b222b7b1c6e8f8aa9326
Evan Burriola <evanburriola12@tamu.edu>
2023-05-27 17:40:28 -0500

    Updated Saturation modifier for all foods.

--- src/main/java/ewee_foods/item/ModFoods.java
+++ src/main/java/ewee_foods/item/ModFoods.java
@@ -10,7 +10,7 @@ public class ModFoods {
     //6000T = 5 min
     public static final FoodProperties CURED_MEAT = (new FoodProperties.Builder())
             .nutrition(4)
-            .saturationMod(1.5F)
+            .saturationMod(0.6F)
             .effect(new MobEffectInstance(MobEffects.NIGHT_VISION, 600, 0), 1.0F)
             .effect(new MobEffectInstance(MobEffects.CONFUSION, 220, 0), 0.5F)
             .meat()
@@ -18,19 +18,19 @@ public class ModFoods {
             .build();
     public static final FoodProperties CHICKEN_FRIED_STEAK = (new FoodProperties.Builder())
             .nutrition(10)
-            .saturationMod(.20F)
+            .saturationMod(.80F)
             .meat()
             .effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 6000, 0), 1.0F)
             .build();
     public static final FoodProperties FRESH_LOAF = (new FoodProperties.Builder())
             .nutrition(5)
-            .saturationMod(.40F)
+            .saturationMod(.60F)
             .effect(new MobEffectInstance(MobEffects.ABSORPTION, 1200, 0), 1.0F)
             .build();
 
     public static final FoodProperties GLAZED_RABBIT = (new FoodProperties.Builder())
-            .nutrition(8)
-            .saturationMod(.25F)
+            .nutrition(6)
+            .saturationMod(.6F)
             .meat()
             .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 800, 0), 1.0F)
             .effect(new MobEffectInstance(MobEffects.JUMP, 800, 1), 1.0F)
@@ -39,28 +39,28 @@ public class ModFoods {
 
     public static final FoodProperties ROAST = (new FoodProperties.Builder())
             .nutrition(12)
-            .saturationMod(0.33F)
+            .saturationMod(1.0F)
             .meat()
             .effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 800,0), 1.0F)
             .effect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 800,0), 1.0F)
             .build();
 
     public static final FoodProperties NUTRITIOUS_SOUP = (new FoodProperties.Builder())
-            .nutrition(8)
-            .saturationMod(0.5F)
+            .nutrition(10)
+            .saturationMod(0.6F)
             .effect(new MobEffectInstance(MobEffects.DIG_SPEED, 6000,5), 1.0F)
             .effect(new MobEffectInstance(MobEffects.WEAKNESS, 6000,3), 1.0F)
             .build();
 
     public static final FoodProperties ROAST_BEEF = (new FoodProperties.Builder())
             .nutrition(8)
-            .saturationMod(0.5F)
+            .saturationMod(0.8F)
             .effect(new MobEffectInstance(MobEffects.LUCK, 6000, 1), 1.0F)
             .build();
 
     public static final FoodProperties SPICY_CHICKEN_SANDWICH = (new FoodProperties.Builder())
             .nutrition(8)
-            .saturationMod(0.25F)
+            .saturationMod(0.8F)
             .effect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 2400, 1), 1.0F)
             .effect(new MobEffectInstance(MobEffects.ABSORPTION, 1200, 0), 1.0F)
             .meat()
@@ -68,22 +68,22 @@ public class ModFoods {
 
     public static final FoodProperties BACON_AND_EGGS = (new FoodProperties.Builder())
             .nutrition(8)
-            .saturationMod(0.25F)
+            .saturationMod(0.8F)
             .effect(new MobEffectInstance(MobEffects.HEALTH_BOOST, 2400, 0), 1.0F)
             .meat()
             .build();
 
     public static final FoodProperties LAMB_BAT = (new FoodProperties.Builder())
-            .nutrition(8)
-            .saturationMod(0.25F)
+            .nutrition(7)
+            .saturationMod(0.7F)
             .effect(new MobEffectInstance(MobEffects.HEAL, 1, 0), 1.0F)
             .meat()
             .fast()
             .build();
 
     public static final FoodProperties SUSHI = (new FoodProperties.Builder())
-            .nutrition(8)
-            .saturationMod(0.25F)
+            .nutrition(6)
+            .saturationMod(0.6F)
             .effect(new MobEffectInstance(MobEffects.DOLPHINS_GRACE, 6000, 0), 1.0F)
             .effect(new MobEffectInstance(MobEffects.WATER_BREATHING, 6000, 0), 1.0F)
             .meat()
